3b5ab1d4e7b70a08f5af6b482af6c8310232ffb4
Bruna <brunloyola02@gmail.com>
2020-02-14 08:19:59 -0300

    fiz o que o prof pediu...

--- teste.c
+++ teste.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 
 int main(){
+    int idade;
 
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Sua idade menos 1: %d\n", (idade - 1));
 
     return 0;
 }
